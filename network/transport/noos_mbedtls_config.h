f5afe5e6df44cb778b48739dcd9ce6852d1a0cfe
Mihail Chindris <mihail.chindris@analog.com>
2020-04-29 09:26:50 +0300

    network:transport: Add config file for mbedtls
    This file is used to configure mbedtls library in order to be used on noos

---
+++ network/transport/noos_mbedtls_config.h
@@ -0,0 +1,82 @@
+#ifndef MBEDTLS_CONFIG_H
+#define MBEDTLS_CONFIG_H
+
+/* TODO refactor this file */
+
+/* This setup works for ADUCM3029 */
+
+//See ssl_ciphersuites.h for some more info
+//#define MBEDTLS_KEY_EXCHANGE_PSK_ENABLED
+#define MBEDTLS_KEY_EXCHANGE_RSA_ENABLED
+//#define MBEDTLS_KEY_EXCHANGE_ECDHE_PSK_ENABLED
+//#define MBEDTLS_KEY_EXCHANGE_RSA_PSK_ENABLED
+//#define MBEDTLS_KEY_EXCHANGE_ECDH_RSA_ENABLED
+//#define MBEDTLS_KEY_EXCHANGE_ECDHE_ECDSA_ENABLED
+//#define MBEDTLS_KEY_EXCHANGE_ECDH_ECDSA_ENABLED
+
+//#define MBEDTLS_ECP_DP_SECP192R1_ENABLED
+//#define MBEDTLS_ECP_DP_SECP224R1_ENABLED
+//#define MBEDTLS_ECP_DP_SECP256R1_ENABLED
+//#define MBEDTLS_ECP_DP_SECP384R1_ENABLED
+//#define MBEDTLS_ECP_DP_SECP521R1_ENABLED
+//#define MBEDTLS_ECP_DP_SECP192K1_ENABLED
+//#define MBEDTLS_ECP_DP_SECP224K1_ENABLED
+//#define MBEDTLS_ECP_DP_SECP256K1_ENABLED
+//#define MBEDTLS_ECP_DP_BP256R1_ENABLED
+//#define MBEDTLS_ECP_DP_BP384R1_ENABLED
+//#define MBEDTLS_ECP_DP_BP512R1_ENABLED
+//#define MBEDTLS_ECP_DP_CURVE25519_ENABLED
+//#define MBEDTLS_ECP_DP_CURVE448_ENABLED 
+
+//#define MBEDTLS_ASN1_WRITE_C
+//#define MBEDTLS_ECP_C
+//#define MBEDTLS_ECDH_C
+//#define MBEDTLS_ECDSA_C
+
+#define MBEDTLS_NO_PLATFORM_ENTROPY
+
+#define MBEDTLS_PK_C
+#define MBEDTLS_PK_PARSE_C
+#define MBEDTLS_ASN1_PARSE_C
+#define MBEDTLS_OID_C
+#define MBEDTLS_PKCS1_V15
+#define MBEDTLS_BASE64_C
+#define MBEDTLS_PEM_PARSE_C
+#define MBEDTLS_CERTS_C
+#define MBEDTLS_RSA_C
+#define MBEDTLS_BIGNUM_C
+#define MBEDTLS_X509_USE_C
+#define MBEDTLS_X509_CRT_PARSE_C
+
+
+/* For debug */
+//#define MBEDTLS_DEBUG_C
+//#define MBEDTLS_ERROR_C
+
+/* mbed TLS feature support */
+#define MBEDTLS_SSL_PROTO_TLS1_2
+
+/* mbed TLS modules */
+#define MBEDTLS_AES_C
+#define MBEDTLS_CCM_C
+#define MBEDTLS_CIPHER_C
+#define MBEDTLS_CTR_DRBG_C
+#define MBEDTLS_ENTROPY_C
+#define MBEDTLS_MD_C
+#define MBEDTLS_SHA256_C
+#define MBEDTLS_SSL_CLI_C
+#define MBEDTLS_SSL_TLS_C
+
+
+/* Config only specified ciphersuites */
+//#define MBEDTLS_SSL_CIPHERSUITES \
+//        MBEDTLS_TLS_PSK_WITH_AES_256_CCM_8,             \
+//        MBEDTLS_TLS_PSK_WITH_AES_128_CCM_8
+
+/* This should be the buffer size in wifi.c
+TODO Think to an other aproach to pass the socket len */
+#define MBEDTLS_SSL_MAX_CONTENT_LEN             3500
+
+#include "mbedtls/check_config.h"
+
+#endif /* MBEDTLS_CONFIG_H */
